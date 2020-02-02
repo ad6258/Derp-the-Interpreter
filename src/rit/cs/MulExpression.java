73c2b26b4d79dd7c638bee2334b980e97cd1446a
Atharva Dhupkar <atharvadhupkar@atharvas-mbp.wireless.rit.edu>
2020-02-02 17:14:18 -0500

    mul and sub work

--- src/rit/cs/MulExpression.java
+++ src/rit/cs/MulExpression.java
@@ -3,13 +3,15 @@ package rit.cs;
 public class MulExpression implements Expression {
     private Expression left, right;
     public MulExpression(Expression left, Expression right){
+        this.left = left;
+        this.right = right;
     }
     @Override
     public int evaluate(){
-        return 0;
+        return this.left.evaluate() * this.right.evaluate();
     }
     @Override
     public String emit(){
-        return null;
+        return "(" + this.left.emit() + " * " + this.right.emit() + ")";
     }
 }
--- src/rit/cs/SubExpression.java
+++ src/rit/cs/SubExpression.java
@@ -3,13 +3,15 @@ package rit.cs;
 public class SubExpression implements Expression {
     private Expression left, right;
     public SubExpression(Expression left, Expression right){
+        this.left = left;
+        this.right = right;
     }
     @Override
     public int evaluate(){
-        return 0;
+        return this.left.evaluate() - this.right.evaluate();
     }
     @Override
     public String emit(){
-        return null;
+        return "(" + this.left.emit() + " - " + this.right.emit() + ")";
     }
 }

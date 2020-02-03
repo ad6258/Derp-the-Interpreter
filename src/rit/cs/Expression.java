865a7eabefe151eab6c2f63835c4ef02fba523f9
Atharva Dhupkar <atharvadhupkar@atharvas-mbp.wireless.rit.edu>
2020-02-03 13:40:46 -0500

    tokenRight does work

--- src/rit/cs/Expression.java
+++ src/rit/cs/Expression.java
@@ -3,4 +3,5 @@ package rit.cs;
 public interface Expression {
     public int evaluate();
     public String emit();
+
 }
--- src/rit/stu/DYI.java
+++ src/rit/stu/DYI.java
@@ -1,4 +1,81 @@
 package rit.stu;
 
+import rit.cs.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class DYI {
+    private Scanner in = new Scanner(System.in);
+    private Expression root;
+    private ArrayList<String> token, tokenLeft, tokenRight;
+    public DYI(Scanner in){
+        this.in = in;
+    }
+    private void makeToken(Scanner in){
+        System.out.print(">");
+        String prefix = in.nextLine();
+        token = new ArrayList<String>(prefix.length());
+        for (int i = 0; i < prefix.length(); i++ ){
+            if(prefix.charAt(i) != ' '){
+                token.add("" + prefix.charAt(i));
+            }
+        }
+        root = parse(token);
+    }
+
+    private Expression parse(ArrayList<String> token) {
+        if(token.get(0).equals("+")){
+            String thingLeft = token.remove(0);
+            tokenLeft = token;
+            String thingRight = token.remove(0);
+            tokenRight = token;
+            return new AddExpression(parse(tokenLeft), parse(tokenRight));
+        }
+        else if(token.get(0).equals("-")){
+            String thingLeft = token.remove(0);
+            tokenLeft = token;
+            String thingRight = token.remove(0);
+            tokenRight = token;
+            return new SubExpression(parse(tokenLeft), parse(tokenRight));
+        }
+        else if(token.get(0).equals("/")){
+            String thingLeft = token.remove(0);
+            tokenLeft = token;
+            String thingRight = token.remove(0);
+            tokenRight = token;
+            return new DivExpression(parse(tokenLeft), parse(tokenRight));
+        }
+        else if(token.get(0).equals("*") || token.get(0).equals("x")){
+            String thingLeft = token.remove(0);
+            tokenLeft = token;
+            String thingRight = token.remove(0);
+            tokenRight = token;
+            return new AddExpression(parse(tokenLeft), parse(tokenRight));
+        }
+        else if(token.get(0).equals("%")){
+            String thingLeft = token.remove(0);
+            tokenLeft = token;
+            String thingRight = token.remove(0);
+            tokenRight = token;
+            return new DivExpression(parse(tokenLeft), parse(tokenRight));
+        }
+        else{
+            return new IntExpression(Integer.parseInt(token.remove(0)));
+        }
+    }
+
+    public void doEverything(){
+        System.out.println("Derp Your Interpreter v1.0");
+        makeToken(in);
+        System.out.println(root.emit());
+        System.out.println(root.evaluate());
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DYI helper = new DYI(in);
+        helper.doEverything();
+        in.close();
+    }
 }

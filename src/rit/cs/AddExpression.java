19ed82768a4f45727bb468a4ba430b9d95d456f0
Atharva Dhupkar <atharvadhupkar@atharvas-mbp.wireless.rit.edu>
2020-02-03 20:24:38 -0500

    works, no comments

--- src/rit/cs/AddExpression.java
+++ src/rit/cs/AddExpression.java
@@ -1,15 +1,40 @@
 package rit.cs;
 
+/**
+ * This class encapsulates fields and methods to perform addition on a tree
+ * AddExpression implements the interface Expression
+ *
+ * @author Atharva Dhupkar ad6258@g.rit.edu
+ */
 public class AddExpression implements Expression {
+    /**
+     * left and right of type Expression are variables for the left and right nodes of "+" node
+     */
     private Expression left, right;
+
+    /**
+     * AddExpression instantiates left(field) and right(field) with the parameters left and right respectively.
+     * @param left left node of type Expression of "+"
+     * @param right right node of type Expression of "+"
+     */
     public AddExpression(Expression left, Expression right){
         this.left = left;
         this.right = right;
     }
+
+    /**
+     * This method adds the left and right nodes after they've been evaluated
+     * @return sum of type int
+     */
     @Override
     public int evaluate(){
         return this.left.evaluate() + this.right.evaluate();
     }
+
+    /**
+     * emit prints the expression that is to be added
+     * @return String of the expression to be printed
+     */
     @Override
     public String emit(){
         return "(" + this.left.emit() + " + " + this.right.emit() + ")";
--- src/rit/stu/DYI.java
+++ src/rit/stu/DYI.java
@@ -3,74 +3,58 @@ package rit.stu;
 import rit.cs.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
+/**
+ *
+ */
 public class DYI {
     private Scanner in = new Scanner(System.in);
     private Expression root;
-    private ArrayList<String> token, tokenLeft, tokenRight;
+    private ArrayList<String> tokens;
     public DYI(Scanner in){
         this.in = in;
     }
     private void makeToken(Scanner in){
         System.out.print(">");
         String prefix = in.nextLine();
-        token = new ArrayList<String>(prefix.length());
+        tokens = new ArrayList<String>(prefix.length());
         for (int i = 0; i < prefix.length(); i++ ){
             if(prefix.charAt(i) != ' '){
-                token.add("" + prefix.charAt(i));
+                tokens.add("" + prefix.charAt(i));
             }
         }
-        root = parse(token);
+        root = parse(tokens);
     }
 
-    private Expression parse(ArrayList<String> token) {
-        if(token.get(0).equals("+")){
-            String thingLeft = token.remove(0);
-            tokenLeft = token;
-            String thingRight = token.remove(0);
-            tokenRight = token;
-            return new AddExpression(parse(tokenLeft), parse(tokenRight));
+    private Expression parse(ArrayList<String> tokens) {
+        String thing = tokens.remove(0);
+        if(thing.equals("+")){
+            return new AddExpression(parse(tokens), parse(tokens));
         }
-        else if(token.get(0).equals("-")){
-            String thingLeft = token.remove(0);
-            tokenLeft = token;
-            String thingRight = token.remove(0);
-            tokenRight = token;
-            return new SubExpression(parse(tokenLeft), parse(tokenRight));
+        else if(thing.equals("-")){
+            return new SubExpression(parse(tokens), parse(tokens));
         }
-        else if(token.get(0).equals("/")){
-            String thingLeft = token.remove(0);
-            tokenLeft = token;
-            String thingRight = token.remove(0);
-            tokenRight = token;
-            return new DivExpression(parse(tokenLeft), parse(tokenRight));
+        else if(thing.equals("/")){
+            return new DivExpression(parse(tokens), parse(tokens));
         }
-        else if(token.get(0).equals("*") || token.get(0).equals("x")){
-            String thingLeft = token.remove(0);
-            tokenLeft = token;
-            String thingRight = token.remove(0);
-            tokenRight = token;
-            return new AddExpression(parse(tokenLeft), parse(tokenRight));
+        else if(thing.equals("*") || thing.equals("x")){
+            return new AddExpression(parse(tokens), parse(tokens));
         }
-        else if(token.get(0).equals("%")){
-            String thingLeft = token.remove(0);
-            tokenLeft = token;
-            String thingRight = token.remove(0);
-            tokenRight = token;
-            return new DivExpression(parse(tokenLeft), parse(tokenRight));
+        else if(thing.equals("%")){
+            return new DivExpression(parse(tokens), parse(tokens));
         }
         else{
-            return new IntExpression(Integer.parseInt(token.remove(0)));
+            return new IntExpression(Integer.parseInt(thing));
         }
     }
 
     public void doEverything(){
         System.out.println("Derp Your Interpreter v1.0");
         makeToken(in);
-        System.out.println(root.emit());
-        System.out.println(root.evaluate());
+        System.out.println("Emit: " + root.emit());
+        System.out.println("Evaluate: " + root.evaluate());
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
